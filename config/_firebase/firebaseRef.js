b994145d0825fe362093ba4895f3de89622b49cd
Mukhlis Adhe Purwanto <adhemukhlis@gmail.com>
2021-03-25 00:28:47 +0700

    firestore revision-2

--- config/_firebase/firebaseRef.js
+++ config/_firebase/firebaseRef.js
@@ -1,4 +1,4 @@
-var firebase = require('firebase')
+var firebase = require( 'firebase' );
 const config = {
 	apiKey: "AIzaSyDkI3uplq9THqQ9019P5oj9DD36oNhKpqk",
 	authDomain: "netlify-express-rest.firebaseapp.com",
@@ -7,8 +7,8 @@ const config = {
 	messagingSenderId: "522808349855",
 	appId: "1:522808349855:web:b9c5510bc1e03b883dfeaa"
 };
-const firebaseInitConfig =  firebase.initializeApp( config )
-const firebaseFirestore = firebaseInitConfig.firestore()
+const firebaseInitConfig = firebase.initializeApp( config );
+const firebaseFirestore = firebaseInitConfig.firestore( );
 export const users = async( ) => {
 	firebaseFirestore.settings({ timestampsInSnapshots: true });
 	firebaseFirestore
--- src/api.js
+++ src/api.js
@@ -1,6 +1,18 @@
 const express = require("express");
-const serverless = require("serverless-http");
-const {users,postUsers} = require("../config/_firebase/firebaseRef")
+// const serverless = require("serverless-http");
+// const {users,postUsers} = require("../config/_firebase/firebaseRef")
+const firebase = require( 'firebase' );
+const config = {
+	apiKey: "AIzaSyDkI3uplq9THqQ9019P5oj9DD36oNhKpqk",
+	authDomain: "netlify-express-rest.firebaseapp.com",
+	projectId: "netlify-express-rest",
+	storageBucket: "netlify-express-rest.appspot.com",
+	messagingSenderId: "522808349855",
+	appId: "1:522808349855:web:b9c5510bc1e03b883dfeaa"
+};
+const firebaseInitConfig = firebase.initializeApp( config );
+const firebaseFirestore = firebaseInitConfig.firestore( );
+
 const app = express();
 const router = express.Router();
 
@@ -11,16 +23,31 @@ router.get("/test", (req, res) => {
   res.send({message:'/test success!'})
 });
 router.get("/users", (req, res) => {
-  users().then((data)=>res.send({  data }))
+  firebaseFirestore.settings({ timestampsInSnapshots: true });
+	firebaseFirestore
+		.collection( 'users' )
+		.orderBy( 'waktu', 'desc' )
+		.get( )
+		.then(snapshot => {
+			const response = snapshot.map(( hasil ) => ({
+				...hasil.data( )
+			}));
+			res.send({response})
+		})
+		.catch(( error ) => {
+			res.send({error})
+		})
 });
 router.post("/users", ( req, res ) => {
   const {username,email} = req.body
-  postUsers({username,email}).then((data) => {
-    res.send({ data });
-  })
+  firebaseFirestore.settings({ timestampsInSnapshots: true });
+	firebaseFirestore
+		.collection( 'users' )
+		.add({username, email, waktu: new Date( )});
+	res.send({ username, email });
 });
 
 app.use(`/.netlify/functions/api`, router);
 
 module.exports = app;
-module.exports.handler = serverless(app);
+// module.exports.handler = serverless(app);

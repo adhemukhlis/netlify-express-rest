f016c457868852454b1d651e9bc6a0c4d15df64a
Mukhlis Adhe Purwanto <adhemukhlis@gmail.com>
2021-05-01 01:16:30 +0700

    heroku error handling

--- src/api.js
+++ src/api.js
@@ -262,8 +262,8 @@ router.delete("/users", ( req, res ) => {
 
 app.use( `/api`, router );
 
-app.listen(process.env.port || 4000, ( ) => {
-	console.log( 'listening api' );
+app.listen(process.env.PORT || 3000, ( ) => {
+	console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
 module.exports = app;
